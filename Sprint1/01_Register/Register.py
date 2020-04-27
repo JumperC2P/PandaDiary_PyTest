561c838e4ff07e8009d25db13f1ceab2798b9cb3
ChemIsProgram <adslbbcc1949@gmail.com>
2020-04-27 22:12:55 +1000

    add firefox and ie testing

--- Sprint1/01_Register/Register.py
+++ Sprint1/01_Register/Register.py
@@ -1,22 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import pathlib
+
 import time
 import json
 
 
 class Register():
 
-    def __init__(self):
-        chrome_options = Options()
-        print (pathlib.Path().absolute())
-        self.driver = webdriver.Chrome(executable_path=((str)(pathlib.Path().absolute()))+'/chromedriver', chrome_options=chrome_options)
-        self.driver.implicitly_wait(10)
-        self.driver.get("https://pandadiary.ts.r.appspot.com")
-
-
-    def start(self, user):
-        driver = self.driver
+    def start(self, driver, user, is_checked):
         try:
             register_link = driver.find_element_by_link_text('Register')
         except:
@@ -30,19 +19,18 @@ class Register():
         driver.find_element_by_name('username').send_keys(user['username'])
         driver.find_element_by_name('email').send_keys(user['email'])
         driver.find_element_by_name('password').send_keys(user['password'])
-        driver.find_element_by_name('confirmPassword').send_keys(user['password'])
-        driver.find_element_by_name('terms').click()
+        driver.find_element_by_name('confirmPassword').send_keys(user['confirmed_password'])
+        if (is_checked):
+            driver.find_element_by_name('terms').click()
+        time.sleep(2)
         submit_button = driver.find_element_by_css_selector("button[type='submit']")
         submit_button.click()
         time.sleep(2)
-        result = driver.find_element_by_id('swal2-title')
-        return result.text
-        
+        result = driver.find_element_by_id('swal2-title').text
+        if (result == "Oops..."):
+            result = driver.find_element_by_id('swal2-content').text
+            
+        time.sleep(2)
+        return result
 
-# if __name__ == '__main__':
-#     user = {
-#         'username': 'Testing',
-#         'email': 'testing@gmail.com',
-#         'password': '12345678'
-#     }
-#     print (Register().start(user))
+        
--- Sprint1/01_Register/RegisterTest.py
+++ Sprint1/01_Register/RegisterTest.py
@@ -1,22 +1,109 @@
 import unittest;
-from Register import Register;
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pathlib
+from Register import Register
+import platform
+
+WEB_URL = "https://pandadiary.ts.r.appspot.com"
 
 class RegisterTest(unittest.TestCase):
+
 
     def setUp(self):
         self.user = {
             'username': 'Testing',
             'email': 'testing@gmail.com',
-            'password': '12345678'
+            'password': '12345678',
+            'confirmed_password': '12345678'
+        }
+        self.user1 = {
+            'username': 'Testingg',
+            'email': 'testing1@gmail.com',
+            'password': '12345678',
+            'confirmed_password': '12345678'
+        }
+        
+        chrome_options = Options()
+        self.chrome_driver = webdriver.Chrome(executable_path=((str)(pathlib.Path().absolute()))+'/chromedriver', chrome_options=chrome_options)
+        self.chrome_driver.implicitly_wait(10)
+        self.chrome_driver.get(WEB_URL)
+
+        self.firefox_driver = webdriver.Firefox(executable_path=((str)(pathlib.Path().absolute()))+'/geckodriver')
+        self.firefox_driver.implicitly_wait(10)
+        self.firefox_driver.get(WEB_URL)
+
+        self.drivers = {
+            'Chrome': self.chrome_driver,
+            'Firefox': self.firefox_driver
         }
 
-    def tearDown(self):
-        self.args = None
+        if (platform.system() == 'Windows'):
+            self.ie_driver = webdriver.Firefox(executable_path=((str)(pathlib.Path().absolute()))+'/IEDriverServer.exe')
+            self.ie_driver.implicitly_wait(10)
+            self.ie_driver.get(WEB_URL)
+            self.drivers['IE'] = self.ie_driver
 
-    def test_register(self):
+
+    def tearDown(self):
+        for k in self.drivers:
+            self.drivers[k].close()
+        return super().tearDown()
+
+    def register_no_capital_name(self, driver):
+        self.user['username'] = 'testing'
+        return Register().start(driver, self.user, True)
+
+    def register_wrong_email(self, driver):
+        self.user['email'] = 'testinggmail.com'
+        return Register().start(driver, self.user, True)
+
+    def register_wrong_password(self, driver):
+        self.user['confirmed_password'] = '1234583930'
+        return Register().start(driver, self.user, True)
+
+    def register_not_checked(self, driver):
+        return Register().start(driver, self.user, False)
+
+    def register_success(self, driver, user):
+        return Register().start(driver, user, True)
+
+    def test_register_no_capital_name(self):
+        expected = "Please make sure details you've entered are correct!"
+        for k in self.drivers:
+            result = self.register_no_capital_name(self.drivers[k])
+            self.assertEqual(expected, result)
+
+    def test_register_wrong_email(self):
+        expected = "Please make sure details you've entered are correct!"
+        for k in self.drivers:
+            result = self.register_wrong_email(self.drivers[k])
+            self.assertEqual(expected, result)
+
+    def test_register_wrong_password(self):
+        expected = "Please make sure details you've entered are correct!"
+        for k in self.drivers:
+            result = self.register_wrong_password(self.drivers[k])
+            self.assertEqual(expected, result)
+
+    def test_register_not_checked(self):
+        expected = "Please make sure you've checked the box!"
+        for k in self.drivers:
+            result = self.register_not_checked(self.drivers[k])
+            self.assertEqual(expected, result)
+
+    def test_register_success(self):
         expected = "Registration successful."
-        result = Register().start(self.user)
-        self.assertEqual(expected, result)
+        is_first = True
+        for k in self.drivers:
+            if is_first:
+                user = self.user
+                is_first = False
+            else:
+                user = self.user1
+
+            result = self.register_success(self.drivers[k], user)
+            self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
